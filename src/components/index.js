a3b37a33e63b9d5520b6211edc8cbcab4b0f927f
Koby Johnson <knj11@vt.edu>
2021-05-16 20:03:09 -0400

    Finished Day 2

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as SearchBar } from './SearchBar';
 export { default as SearchResults } from './SearchResults';
 export { default as DeckList } from './DeckList';
+export { default as Card } from './Card';
